0512f91249bcb4b7ab5c38a9cea12582334d879a
JunWonOh <junoh9847@gmail.com>
2020-12-27 22:18:33 -0500

    Added frame counter functionality

--- PianoTrainer/app/src/main/java/com/example/pianotrainer/MainActivity.java
+++ PianoTrainer/app/src/main/java/com/example/pianotrainer/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.pianotrainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int frameCount = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void upArrowClick (View view) {
+        TextView frameCounter = findViewById(R.id.frameCounter);
+        frameCount = frameCount + 1;
+        frameCounter.setText(String.valueOf(frameCount));
+    }
+
+    public void downArrowClick (View view) {
+        TextView frameCounter = findViewById(R.id.frameCounter);
+        if (frameCount >= 1) {
+            frameCount = frameCount - 1;
+        }
+        frameCounter.setText(String.valueOf(frameCount));
     }
 }
