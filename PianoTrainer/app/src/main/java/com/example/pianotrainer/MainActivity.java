74ecd3fd6fcbb3ab326970654684a1b7fadc24eb
JunWonOh <junoh9847@gmail.com>
2020-12-29 22:52:07 -0500

    added toggle function for every ImageView

--- PianoTrainer/app/src/main/java/com/example/pianotrainer/MainActivity.java
+++ PianoTrainer/app/src/main/java/com/example/pianotrainer/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -23,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
         //piano.cNote();
     }
 
-    public void cNote (View view) {
+    //TODO: Images need to "wake up" before being pressed
+    public void _middleC4Note (View view) {
         ImageView keyOne = (ImageView) findViewById(R.id.keyOne);
         Integer integer = (Integer) keyOne.getTag();
         integer = integer == null ? 0 : integer;
@@ -34,6 +36,222 @@ public class MainActivity extends AppCompatActivity {
         } else {
             keyOne.setImageResource(R.mipmap.one);
             keyOne.setTag(R.mipmap.one);
+        }
+
+//        keyOne.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick (View view) {
+//            }
+//        });
+    }
+    public void _D4Note (View view) {
+        ImageView keyTwo = (ImageView) findViewById(R.id.keyTwo);
+        Integer integer = (Integer) keyTwo.getTag();
+        integer = integer == null ? 0 : integer;
+
+        if (integer == R.mipmap.two) {
+            keyTwo.setImageResource(R.mipmap.twoclicked);
+            keyTwo.setTag(R.mipmap.twoclicked);
+        } else {
+            keyTwo.setImageResource(R.mipmap.two);
+            keyTwo.setTag(R.mipmap.two);
+        }
+    }
+    public void _E4Note (View view) {
+        ImageView keyThree = (ImageView) findViewById(R.id.keyThree);
+        Integer integer = (Integer) keyThree.getTag();
+        integer = integer == null ? 0 : integer;
+
+        if (integer == R.mipmap.three) {
+            keyThree.setImageResource(R.mipmap.threeclicked);
+            keyThree.setTag(R.mipmap.threeclicked);
+        } else {
+            keyThree.setImageResource(R.mipmap.three);
+            keyThree.setTag(R.mipmap.three);
+        }
+    }
+    public void _F4Note (View view) {
+        ImageView keyFour = (ImageView) findViewById(R.id.keyFour);
+        Integer integer = (Integer) keyFour.getTag();
+        integer = integer == null ? 0 : integer;
+
+        if (integer == R.mipmap.one) {
+            keyFour.setImageResource(R.mipmap.oneclicked);
+            keyFour.setTag(R.mipmap.oneclicked);
+        } else {
+            keyFour.setImageResource(R.mipmap.one);
+            keyFour.setTag(R.mipmap.one);
+        }
+    }
+    public void _G4Note (View view) {
+        ImageView keyFive = (ImageView) findViewById(R.id.keyFive);
+        Integer integer = (Integer) keyFive.getTag();
+        integer = integer == null ? 0 : integer;
+
+        if (integer == R.mipmap.two) {
+            keyFive.setImageResource(R.mipmap.twoclicked);
+            keyFive.setTag(R.mipmap.twoclicked);
+        } else {
+            keyFive.setImageResource(R.mipmap.two);
+            keyFive.setTag(R.mipmap.two);
+        }
+    }
+    public void _A4Note (View view) {
+        ImageView keySix = (ImageView) findViewById(R.id.keySix);
+        Integer integer = (Integer) keySix.getTag();
+        integer = integer == null ? 0 : integer;
+
+        if (integer == R.mipmap.two) {
+            keySix.setImageResource(R.mipmap.twoclicked);
+            keySix.setTag(R.mipmap.twoclicked);
+        } else {
+            keySix.setImageResource(R.mipmap.two);
+            keySix.setTag(R.mipmap.two);
+        }
+    }
+    public void _B4Note (View view) {
+        ImageView keySeven = (ImageView) findViewById(R.id.keySeven);
+        Integer integer = (Integer) keySeven.getTag();
+        integer = integer == null ? 0 : integer;
+
+        if (integer == R.mipmap.three) {
+            keySeven.setImageResource(R.mipmap.threeclicked);
+            keySeven.setTag(R.mipmap.threeclicked);
+        } else {
+            keySeven.setImageResource(R.mipmap.three);
+            keySeven.setTag(R.mipmap.three);
+        }
+    }
+
+    public void _C5Note (View view) {
+        ImageView key = (ImageView) findViewById(R.id.keyEight);
+        Integer integer = (Integer) key.getTag();
+        integer = integer == null ? 0 : integer;
+
+        if (integer == R.mipmap.one) {
+            key.setImageResource(R.mipmap.oneclicked);
+            key.setTag(R.mipmap.oneclicked);
+        } else {
+            key.setImageResource(R.mipmap.one);
+            key.setTag(R.mipmap.one);
+        }
+    }
+    public void _D5Note (View view) {
+        ImageView key = (ImageView) findViewById(R.id.keyNine);
+        Integer integer = (Integer) key.getTag();
+        integer = integer == null ? 0 : integer;
+
+        if (integer == R.mipmap.two) {
+            key.setImageResource(R.mipmap.twoclicked);
+            key.setTag(R.mipmap.twoclicked);
+        } else {
+            key.setImageResource(R.mipmap.two);
+            key.setTag(R.mipmap.two);
+        }
+    }
+    public void _E5Note (View view) {
+        ImageView key = (ImageView) findViewById(R.id.keyTen);
+        Integer integer = (Integer) key.getTag();
+        integer = integer == null ? 0 : integer;
+
+        if (integer == R.mipmap.three) {
+            key.setImageResource(R.mipmap.threeclicked);
+            key.setTag(R.mipmap.threeclicked);
+        } else {
+            key.setImageResource(R.mipmap.three);
+            key.setTag(R.mipmap.three);
+        }
+    }
+    public void _F5Note (View view) {
+        ImageView key = (ImageView) findViewById(R.id.keyEleven);
+        Integer integer = (Integer) key.getTag();
+        integer = integer == null ? 0 : integer;
+
+        if (integer == R.mipmap.one) {
+            key.setImageResource(R.mipmap.oneclicked);
+            key.setTag(R.mipmap.oneclicked);
+        } else {
+            key.setImageResource(R.mipmap.one);
+            key.setTag(R.mipmap.one);
+        }
+    }
+    public void _G5Note (View view) {
+        ImageView key = (ImageView) findViewById(R.id.keyTwelve);
+        Integer integer = (Integer) key.getTag();
+        integer = integer == null ? 0 : integer;
+
+        if (integer == R.mipmap.two) {
+            key.setImageResource(R.mipmap.twoclicked);
+            key.setTag(R.mipmap.twoclicked);
+        } else {
+            key.setImageResource(R.mipmap.two);
+            key.setTag(R.mipmap.two);
+        }
+    }
+    public void _A5Note (View view) {
+        ImageView key = (ImageView) findViewById(R.id.keyThirteen);
+        Integer integer = (Integer) key.getTag();
+        integer = integer == null ? 0 : integer;
+
+        if (integer == R.mipmap.two) {
+            key.setImageResource(R.mipmap.twoclicked);
+            key.setTag(R.mipmap.twoclicked);
+        } else {
+            key.setImageResource(R.mipmap.two);
+            key.setTag(R.mipmap.two);
+        }
+    }
+    public void _B5Note (View view) {
+        ImageView key = (ImageView) findViewById(R.id.keyFourteen);
+        Integer integer = (Integer) key.getTag();
+        integer = integer == null ? 0 : integer;
+
+        if (integer == R.mipmap.three) {
+            key.setImageResource(R.mipmap.threeclicked);
+            key.setTag(R.mipmap.threeclicked);
+        } else {
+            key.setImageResource(R.mipmap.three);
+            key.setTag(R.mipmap.three);
+        }
+    }
+
+    public void _C6Note (View view) {
+        ImageView key = (ImageView) findViewById(R.id.keyFifteen);
+        Integer integer = (Integer) key.getTag();
+        integer = integer == null ? 0 : integer;
+
+        if (integer == R.mipmap.one) {
+            key.setImageResource(R.mipmap.oneclicked);
+            key.setTag(R.mipmap.oneclicked);
+        } else {
+            key.setImageResource(R.mipmap.one);
+            key.setTag(R.mipmap.one);
+        }
+    }
+    public void _D6Note (View view) {
+        ImageView key = (ImageView) findViewById(R.id.keySixteen);
+        Integer integer = (Integer) key.getTag();
+        integer = integer == null ? 0 : integer;
+
+        if (integer == R.mipmap.two) {
+            key.setImageResource(R.mipmap.twoclicked);
+            key.setTag(R.mipmap.twoclicked);
+        } else {
+            key.setImageResource(R.mipmap.two);
+            key.setTag(R.mipmap.two);
+        }
+    }
+    public void _E6Note (View view) {
+        ImageView key = (ImageView) findViewById(R.id.keySeventeen);
+        Integer integer = (Integer) key.getTag();
+        integer = integer == null ? 0 : integer;
+
+        if (integer == R.mipmap.three) {
+            key.setImageResource(R.mipmap.threeclicked);
+            key.setTag(R.mipmap.threeclicked);
+        } else {
+            key.setImageResource(R.mipmap.three);
+            key.setTag(R.mipmap.three);
         }
     }
 
